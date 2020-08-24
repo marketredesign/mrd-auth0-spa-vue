bcd525900d3cc985ddc39ee7f802080029e78726
Marijn van der Horst <marijn.vander.horst@marketredesign.com>
2020-08-24 16:08:54 +0200

    Add permission guard

--- src/authGuard.js
+++ src/authGuard.js
@@ -1,5 +1,12 @@
 import { getInstance } from "./index";
 
+/**
+ * Verifies the user is authenticated and redirects to login page if not.
+ *
+ * @param to
+ * @param from
+ * @param next
+ */
 export const authGuard = (to, from, next) => {
     const authService = getInstance();
 
@@ -24,4 +31,46 @@ export const authGuard = (to, from, next) => {
             return fn();
         }
     });
+};
+
+/**
+ * Verifies the user is authenticated and has the provided permission. If the user is not authenticated, they will
+ * be redirected to the login page. If the user doesn't have the provided permission, the route change is not permitted.
+ * @param permission
+ */
+export const permissionGuard = (permission) => {
+    return (to, from, next) => {
+        const authService = getInstance();
+
+        const fn = async () => {
+            // If the user is not authenticated, redirect them to login.
+            if (!authService.state.isAuthenticated) {
+                authService.loginWithRedirect({ appState: { targetUrl: to.fullPath } });
+                return;
+            }
+
+            // Get all claims in the JWT.
+            const claims = await authService.getIdTokenClaims();
+
+            if (!claims['https://marketredesign.com/permissions'].includes(permission)) {
+                // Permission is not included in JWT, so don't allow the route change.
+                return next(false);
+            }
+
+            // User is authenticated and has the required permission. Allow route change.
+            return next();
+        };
+
+        // If loading has already finished, check our auth state using `fn()`
+        if (!authService.state.loading) {
+            return fn();
+        }
+
+        // Watch for the loading property to change before we check isAuthenticated
+        authService.storeVM.$watch("loading", loading => {
+            if (loading === false) {
+                return fn();
+            }
+        });
+    };
 };
